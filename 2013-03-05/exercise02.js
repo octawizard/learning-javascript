d70b37bfb0f0996ee586285664e810e7760afff6
octawizard <roberto.manca.89@gmail.com>
2013-03-05 18:21:37 +0100

    update exercise02.js

--- 2013-03-05/exercise02.js
+++ 2013-03-05/exercise02.js
@@ -4,7 +4,10 @@ var a;
 for (j=1; j<=10;j++){
 	a = "";
 	for (i = 1; i <= 10; i +=1) {
-  		a += j*i+",\t";
+		if (i === 10)
+			a += j*i+"\t";	 
+		else
+			a += j*i+",\t";
 	}
 	console.log(a+"\n")
 }
